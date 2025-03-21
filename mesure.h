bef75d8da2607faaca77d1fca952b090ec140bff
Nirij3m <nirina.macon@isen-ouest.yncrea.fr>
2025-03-21 10:25:55 +0100

    mod

--- mesure.h
+++ mesure.h
@@ -5,11 +5,8 @@
 #include "math.h"
 
 
-
-double get_time_ns();
 oxy mesureTest(char* filename);
 oxy mesure(absorp input, int* nmb_ech, float* prevAC, float* rsir ,float* max_ac_r, float* min_ac_r, float* max_ac_ir, float* min_ac_ir);
-void printTimeBuf(uint size, float* buf);
 
 #endif
 
